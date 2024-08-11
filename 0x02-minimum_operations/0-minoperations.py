997e26a7431f6efa084a3764228bcfea20fe11d1
Midorichie <hamsohood@gmail.com>
2024-08-11 09:37:00 +0100

    Update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,23 @@
-#!/usr/bin/python3
-""" Task for 0-minoperations"""
-
-
 def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to result in exactly n H characters.
+
+    :param n: The desired number of H characters
+    :return: The minimum number of operations, or 0 if n is impossible to achieve
+    """
     if n <= 1:
         return 0
 
     operations = 0
-    divisor = 2
+    factor = 2
 
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
 
+    if n > 1:
+        operations += n
 
     return operations
